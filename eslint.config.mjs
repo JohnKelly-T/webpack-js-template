d6a3167e35f332bc2decf8586e1a50cf09f7c45c
john-kelly <johnkellytemena@gmail.com>
2025-04-08 18:05:46 +0800

    Add custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,10 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+	},
 ]);
