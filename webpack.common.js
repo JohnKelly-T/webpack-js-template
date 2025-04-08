10ab37e151f7f5b4d6b062b41a717f5937de491c
john-kelly <johnkellytemena@gmail.com>
2025-04-08 19:44:47 +0800

    Format code with Prettier

--- webpack.common.js
+++ webpack.common.js
@@ -29,8 +29,8 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },      
+        type: "asset/resource",
+      },
     ],
   },
 };
